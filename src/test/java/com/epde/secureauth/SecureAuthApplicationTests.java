a6a468514a0c9c9a824258f728800413dfad8a0c
Md. Mosfikur Rahman <mdmosfikurrahman.cse@gmail.com>
2023-04-27 22:30:14 +0600

    Initial commit  - Built Project in SpringBoot as Maven Project  - Java Version: 11  - SpringBoot Version: 2.7.11

---
+++ src/test/java/com/epde/secureauth/SecureAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epde.secureauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
