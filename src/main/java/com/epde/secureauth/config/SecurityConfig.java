c21ceb56613e6cf22e310242e4ac092ee03b08a2
Md. Mosfikur Rahman <mosfikur.rahman@bjitgroup.com>
2023-06-01 12:25:37 +0600

    SecureAuth: Basic  - BasicAuth Implemented

--- src/main/java/com/epde/secureauth/config/SecurityConfig.java
+++ src/main/java/com/epde/secureauth/config/SecurityConfig.java
@@ -1,8 +1,6 @@
 package com.epde.secureauth.config;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,23 +12,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        /*
-        [These are DEPRECATED! We should use the rest of below!]
-
-        http.authorizeHttpRequests().anyRequest().authenticated();
-        http.formLogin();
-        http.httpBasic();
-         */
-
-        http
-                .authorizeHttpRequests((authorizeRequests) ->
-                        authorizeRequests
-                                .anyRequest()
-                                .authenticated()
-                );
-
-        http.formLogin(Customizer.withDefaults());
-        http.httpBasic(Customizer.withDefaults());
+        http.authorizeHttpRequests((authorizeRequests) ->
+                authorizeRequests
+                        .anyRequest()
+                        .authenticated())
+                .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
